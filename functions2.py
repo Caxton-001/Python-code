caa233c12f012d53d326014d988a046f3d19a90e
Caxton-001 <81168947+Caxton-001@users.noreply.github.com>
2022-08-12 16:06:48 +0300

    Create functions2.py

---
+++ functions2.py
@@ -0,0 +1,9 @@
+print("Beginning of conversation")
+name = "Caxton"
+def Enter_name(name):
+    name = input("Enter your name: ")
+    print(name)
+
+Enter_name(name)
+print(name)
+print("End of conversation")
